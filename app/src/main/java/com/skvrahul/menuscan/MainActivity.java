792a1f4cfedaea77728177d488aa15ae5eafad1e
Rahul Sathanapalli <skvrahul@gmail.com>
2017-06-25 01:04:26 +0530

    major updates

--- app/src/main/java/com/skvrahul/menuscan/MainActivity.java
+++ app/src/main/java/com/skvrahul/menuscan/MainActivity.java
@@ -1,13 +1,81 @@
 package com.skvrahul.menuscan;
 
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.scanlibrary.ScanActivity;
+import com.scanlibrary.ScanConstants;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
-
+    int REQUEST_CODE = 99;
+    int preferences = ScanConstants.OPEN_CAMERA;
+    Button scanButton;
+    ImageView imageView;
+    Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        scanButton = (Button)findViewById(R.id.scanButton);
+        imageView = (ImageView)findViewById(R.id.scannedImageView);
+        scanButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (checkSelfPermission(Manifest.permission.CAMERA)
+                        != PackageManager.PERMISSION_GRANTED) {
+
+                    requestPermissions(new String[]{Manifest.permission.CAMERA},
+                            97);
+                }
+                intent = new Intent(getApplicationContext(),ScanActivity.class);
+                intent.putExtra(ScanConstants.OPEN_INTENT_PREFERENCE, preferences);
+
+            }
+        });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode== REQUEST_CODE && resultCode== Activity.RESULT_OK){
+            Uri uri = data.getExtras().getParcelable(ScanConstants.SCANNED_RESULT);
+            Bitmap bitmap = null;
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(),uri);
+                getContentResolver().delete(uri, null, null);
+                imageView.setImageBitmap(bitmap);
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == 97) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Now user should be able to use camera
+                startActivityForResult(intent, REQUEST_CODE);
+            }
+            else {
+                // Your app will not have this permission. Turn off all functions
+                // that require this permission or it will force close like your
+                // original question
+            }
+        }
     }
 }
